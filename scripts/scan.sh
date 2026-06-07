3c91643238b82ce26372441133f34911466ead6d
yahiaxbr <bourbia.yahia0@gmail.com>
2026-06-07 18:40:40 +0100

    K: squashing the commits of the scripts

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,10 @@
-# TODO: write the script
+echo "\n======= SCANNING FOR CREDIT CARD NUMBER ======="
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= CREDIT CARD COMPLETE ======="
+echo "\n======= SCANNING FOR SOCIAL SECURITY NUMBER ======="
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= SOCIAL SECURITY NUMBER  COMPLETE ======="
+echo "\n======= SCANNING FOR PHONE NUMBER ======="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= PHONE NUMBER  COMPLETE ======="
